056c9b59b466264c38ece052a8cab09f345c6861
Mohamed Alie Kamara <63104472+Kmohamedalie@users.noreply.github.com>
2023-11-16 16:37:50 +0100

    Update penguins_streamlit.py

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -17,9 +17,10 @@ st.write(
      Use the form below to get started!"""
 )
 
-password_guess = st.text_input("What is the Password?")
+"""password_guess = st.text_input("What is the Password?")
 if password_guess != "streamlit_is_great":
     st.stop()
+"""
 
 
 penguin_file = st.file_uploader("Upload your own penguin data")
